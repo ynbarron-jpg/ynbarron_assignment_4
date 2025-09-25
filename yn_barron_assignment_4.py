690594f8fdd3e477704ff8d71c73da025da6b988
ynbarron-jpg <ynbarron@aggies.ncat.edu>
2025-09-25 18:22:15 -0400

    Update yn_barron_assignment_4.py

--- yn_barron_assignment_4.py
+++ yn_barron_assignment_4.py
@@ -1,16 +1,17 @@
-student_name = "Yahzir Barron"
-current_gpa = 4.0
+student_name = "Yahzir Barron" 
+current_gpa = 4.0 
 study_hours = 20
 social_points = 50
-stress_level = 100
-# displaying game welcome stats
+stress_level = 101 
+#displaying game welcome stats
 print("Welcome to my college life.")
 print("")
 print(f"Current GPA: {current_gpa}")
 print(f"Study hours: {study_hours}")
 print(f"Social points: {social_points}")
 print(f" stress Level.. {stress_level}")
-
+#TEST 1 cases
+#Adding in part 2 commits..
 print("Choose your recovery time:")
 print("A)  8 hours")
 print("B) 10 hours")
@@ -27,7 +28,13 @@ elif choice == "B":
 
 
 elif choice == "C":
-    if current_gpa >= 2.7:
-        stress_level = stress_level - social_points
-else:
-    print("YOU FAILED CLASS...")
+    if stress_level >= 100: 
+        print("YOU FAILED CLASS...")
+    elif current_gpa >= 2.7:
+        stress_level = stress_level + 5
+
+#Step 3: Study strategy Decisiom 
+#Player chooses a recovery time and it correspondes to gpa output
+#Also shows membership operators
+#when player chooses recovery time it will lead to their gpa outcome to see if they will graduate.
+Recvoery_options = []
