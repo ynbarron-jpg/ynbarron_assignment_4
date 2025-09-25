783335a8e74f34abe65fc9c2a4bf2436134a100f
ynbarron-jpg <ynbarron@aggies.ncat.edu>
2025-09-25 17:43:17 -0400

    Add files via upload
    Pass Test Case 2: Course planning with if/elif/else and comparison operators

--- yn_barron_assignment_4.py
+++ yn_barron_assignment_4.py
@@ -11,3 +11,23 @@ print(f"Study hours: {study_hours}")
 print(f"Social points: {social_points}")
 print(f" stress Level.. {stress_level}")
 
+print("Choose your recovery time:")
+print("A)  8hours")
+print("B) 10 hours")
+print("C)  4 hours")
+choice = input("Enter choice: ")
+
+if choice == "A":
+    if current_gpa >= 3.5:
+        stress_level = stress_level - 5
+
+elif choice == "B":
+    if current_gpa >= 3.0:
+        study_hours = study_hours + 3
+
+
+elif choice == "C":
+    if current_gpa >= 2.7:
+        stress_level = stress_level - social_points
+else:
+    print("YOU FAILED CLASS...")
