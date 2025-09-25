9f017a7af888cfbfddbefce9bfb8ae5a1707c740
ynbarron-jpg <ynbarron@aggies.ncat.edu>
2025-09-25 17:31:10 -0400

    Add files via upload
    Pass Test Case 1: Initial game setup with required variables

---
+++ yn_barron_assignment_4.py
@@ -0,0 +1,13 @@
+student_name = "Yahzir Barron"
+current_gpa = 4.0
+study_hours = 20
+social_points = 50
+stress_level = 100
+# displaying game welcome stats
+print("Welcome to my college life.")
+print("")
+print(f"Current GPA: {current_gpa}")
+print(f"Study hours: {study_hours}")
+print(f"Social points: {social_points}")
+print(f" stress Level.. {stress_level}")
+
